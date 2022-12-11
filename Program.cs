9ed24de5d963a85f5604cd1d66341031c0a8ddd7
atemengue <moafembe@gmail.com>
2022-12-11 06:47:14 +0100

    getting started c#

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+int amount = 1234;
+int months = 12;
+
+int yearlyWage = CalculateYearLyWage(amount, months);
+
+Console.Write($"Yearly wage: {yearlyWage}");
+
+Console.ReadLine();
+
+static int CalculateYearLyWage(int monthyWage, int numberOfMonthsWorked)
+{
+  Console.WriteLine($"Yearly wage: {monthyWage * numberOfMonthsWorked}");
+
+  return monthyWage * numberOfMonthsWorked;
+}
