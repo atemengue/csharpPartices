97341f4f07d9680ee8b723709c63b31d0f4465ff
atemengue <moafembe@gmail.com>
2022-12-11 07:21:22 +0100

    worked with methods

--- Utilities.cs
+++ Utilities.cs
@@ -14,5 +14,20 @@ namespace Demo
       return monthyWage * numberOfMonthsWorked;
     }
 
+    public static int CalculateYearLyWage(int monthyWage, int numberOfMonthsWorked, int bonus)
+    {
+      Console.WriteLine($"Yearly wage: {monthyWage * numberOfMonthsWorked + bonus}");
+      return monthyWage * numberOfMonthsWorked + bonus;
+    }
+
+    public static int AddNumbers(int a, int b, int c = 100)
+    {
+      return a + b + c;
+    }
+
+    public static int CalculateYearLyWageExpressionBodied(int monthyWage, int numberOfMonthsWorked, int bonus)
+     => monthyWage * numberOfMonthsWorked + bonus;
+
+
   }
 }
